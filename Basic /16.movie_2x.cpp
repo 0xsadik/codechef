34588333979a3d056c23c05aaa0c51eb103aaf90
0xsadik <torikussadik404@gmail.com>
2023-07-17 05:57:47 +0600

    .

---
+++ Basic /16.movie_2x.cpp
@@ -0,0 +1,14 @@
+// watching movie at 2x
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+
+    cout << (x - y) + (y / 2) << endl;
+
+    return 0;
+}
---
+++ practice/movie2x.cpp
@@ -0,0 +1,13 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+
+    cout << (x - y) + (y / 2) << endl;
+
+    return 0;
+}
