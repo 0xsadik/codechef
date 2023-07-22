fe5c2600f745eb5b3e3a5d66543de949528738b8
0xsadik <torikussadik404@gmail.com>
2023-07-23 01:18:06 +0600

    .

---
+++ Math/44.oneful_pair.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, res;
+	cin >> a >> b;
+	res = (a + b) + (a * b);
+	if (res == 111) {
+	    cout << "Yes\n";
+	}
+	else {
+	    cout << "No\n";
+	}
+	return 0;
+}
+g
