f1b1fad915ce923a09f56dc22bf3fe5035ae5b7e
0xsadik <torikussadik404@gmail.com>
2023-09-15 14:29:17 +0600

    .

---
+++ bash/variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# price_per_apple = 5
+# echo "The price of an apple today is: ${price_per_apple}"
+
+
+# ls
+
+echo "i am learning bash"
---
+++ codeforces/practice/prac.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    int i = 3;
+    cout << arr[i] << endl;
+
+    return 0;
+}
