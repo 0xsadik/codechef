0dd2d439a0ad2e62224b30e8e612ffc35537563c
0xsadik <torikussadik404@gmail.com>
2023-10-29 06:07:13 +0600

    .

---
+++ CPS/Classes/LightOJ/greetings.cpp
@@ -0,0 +1,19 @@
+// greetings from light oj
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int tcase = 1; tcase <= t; tcase++) {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case " << tcase << ": " << a + b << '\n';
+    }
+
+    return 0;
+}
+
+// https://youtu.be/7B_v780IfGI?list=PLoa_roVVsxA2Rqa21grJ4jovQW3kFc3SE&t=2596
