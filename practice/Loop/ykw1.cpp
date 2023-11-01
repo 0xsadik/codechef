dadd9315661d5120b753b15eca8933404ce1528c
0xsadik <torikussadik404@gmail.com>
2023-11-01 21:57:04 +0600

    .

---
+++ practice/Loop/ykw1.cpp
@@ -0,0 +1,66 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     // understanding blocks
+//     int x = 5;
+//     {
+//         // it will execute first 
+//         int y = 10;
+//         cout << "y = " << y << '\n';
+//     }
+//     cout << "x = " << x << '\n';
+
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // understanding blocks
+    int x = 5;
+    {
+        int y = 10;
+        cout << "y = " << y << '\n';
+        cout << "x = " << x << '\n';
+    }
+    cout << "x = " << x << '\n';
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+    
+// }
