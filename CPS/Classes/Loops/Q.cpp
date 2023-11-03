8c1b2092a6a5b4df75ad6e3dec164874c22995cc
0xsadik <torikussadik404@gmail.com>
2023-11-03 23:48:54 +0600

    .

---
+++ CPS/Classes/Loops/Q.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--) {
+        string n;
+        cin >> n;
+        for (int i = n.size() - 1;  i >= 0; i--) {
+            cout << n[i]  << ' ';
+        }
+        cout << '\n';
+    }
+}
