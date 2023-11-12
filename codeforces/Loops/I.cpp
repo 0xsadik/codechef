843b06e2b22bf6045c06717eeff637715adb71ad
0xsadik <torikussadik404@gmail.com>
2023-11-12 21:21:14 +0600

    <.>

--- codeforces/Loops/I.cpp
+++ codeforces/Loops/I.cpp
@@ -5,20 +5,5 @@ using namespace std;
 
 int main()
 {
-    string a, b;
-    cin >> a;
-
-    for (int i = a.size() - 1; i >= 0; i++) {
-        b+=a[i];
-    }
-    int a1, b2;
-    a1 = stoi(a);
-    b2 = stoi(b);
-    cout << b2 << '\n';
-    if (a1 == b2) {
-        cout << "YES\n";
-    }
-    else {
-        cout << "NO\n";
-    }
+    
 }
