23fab00ed34e4a0b7022681ca527b0613e88984e
0xsadik <torikussadik404@gmail.com>
2023-11-21 16:20:16 +0600

    <.>

--- codeforces/Loops/U.cpp
+++ codeforces/Loops/U.cpp
@@ -8,14 +8,7 @@ int main()
     int n, a, b, sum = 0, tsum = 0;
     cin >> n >> a >> b;
 
-    for (int i = 1; i <= n; i++ ) {
-        while(i > 0) {
-            sum += i % 10;
-            i /= 10;
-            if (sum >= a && sum <= b) {
-                tsum += i;
-            }
-        }
-    }
+    int num = 0;
+    
     cout << tsum << '\n';
 }
