6bb32da9b24486cfd41cc29583a6c9c93123653d
0xsadik <torikussadik404@gmail.com>
2023-11-21 18:17:17 +0600

    <.>

--- codeforces/contest1/D.cpp
+++ codeforces/contest1/D.cpp
@@ -65,41 +65,63 @@
 
 
 
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     long long a, b, c, d, s1, s2, s3, s4, s5, s6;
+//     cin >> a >> b >> c;
+
+//     s1 = a + b - c;
+//     s2 = a + (b * c);
+//     s3 = a - b + c;
+//     s4 = a - ( b * c);
+//     s5 = (a * b) + c;
+//     s6 = (a * b) - c;
+
+//     if (s1 == d) {
+//         cout << "YES\n";
+//     }
+//     else if (s2 == d) {
+//         cout << "YES\n";
+//     }
+//     else if (s3 == d) {
+//         cout << "YES\n";
+//     }
+//     else if (s4 == d) {
+//         cout << "YES\n";
+//     }
+//     else if (s5 == d) {
+//         cout << "YES\n";
+//     }
+//     else if (s6 == d) {
+//         cout << "YES\n";
+//     }
+//     else {
+//         cout << "NO\n";
+//     }
+//     return 0;
+// }
+
+
+
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
 
-int main()
-{
-    long long a, b, c, d, s1, s2, s3, s4, s5, s6;
-    cin >> a >> b >> c;
+int main() {
+    long long a, b, c, d;
+    cin >> a >> b >> c >> d;
 
-    s1 = a + b - c;
-    s2 = a + (b * c);
-    s3 = a - b + c;
-    s4 = a - ( b * c);
-    s5 = (a * b) + c;
-    s6 = (a * b) - c;
+    // Try all possible combinations of arithmetic operators
+    if (a + b + c == d || a + b - c == d || a - b + c == d || a - b - c == d ||
+        a * b * c == d || a * b / c == d || a / b * c == d || a / b / c == d) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
 
-    if (s1 == d) {
-        cout << "YES\n";
-    }
-    else if (s2 == d) {
-        cout << "YES\n";
-    }
-    else if (s3 == d) {
-        cout << "YES\n";
-    }
-    else if (s4 == d) {
-        cout << "YES\n";
-    }
-    else if (s5 == d) {
-        cout << "YES\n";
-    }
-    else if (s6 == d) {
-        cout << "YES\n";
-    }
-    else {
-        cout << "NO\n";
-    }
     return 0;
 }
