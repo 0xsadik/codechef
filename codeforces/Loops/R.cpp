06931055bd764abbbb76ec64b046e645371e96cf
0xsadik <torikussadik404@gmail.com>
2023-11-21 14:57:48 +0600

    <.>

--- codeforces/Loops/R.cpp
+++ codeforces/Loops/R.cpp
@@ -1,63 +1,28 @@
 // sequence of numbers and sum 
 
-// #include<iostream>
-// using namespace std;
-
-// int main()
-// {
-//     while (true) {
-//         int n, m, sum = 0;
-//         cin >> n >> m;
-//         if (n <= 0 || m <= 0){
-//             break;
-//         }
-
-//         if (n > m) {
-//             for (int i = m; i <= n; i++) {
-//                 cout << i << ' ';
-//                 sum += i;
-//             }
-//         }
-//         else {
-//             for (int i = n; i <= m; i++) {
-//                 cout << i << ' ';
-//                 sum += i;
-//             }
-//         }
-//         cout << "sum =" << sum << '\n';
-//     }
-//     return 0;
-// }
-
-
-
-
-// let's try another way
-
-
 #include<iostream>
 using namespace std;
-
-
 
 int main()
 {
     while (true) {
         int n, m, sum = 0;
         cin >> n >> m;
-
-        int tmp;
-        tmp = n;
-        n = m;
-        m = tmp;
-
         if (n <= 0 || m <= 0){
             break;
         }
 
-        for (int i = n; i <= m; i++) {
-            cout << i << ' ';
-            sum += i;
+        if (n > m) {
+            for (int i = m; i <= n; i++) {
+                cout << i << ' ';
+                sum += i;
+            }
+        }
+        else {
+            for (int i = n; i <= m; i++) {
+                cout << i << ' ';
+                sum += i;
+            }
         }
         cout << "sum =" << sum << '\n';
     }
