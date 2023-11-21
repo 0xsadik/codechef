4dd87fd12dac41dd683cb5534e5ef9ec62bd633f
0xsadik <torikussadik404@gmail.com>
2023-11-21 16:35:00 +0600

    <.>

--- codeforces/Loops/U.cpp
+++ codeforces/Loops/U.cpp
@@ -1,55 +1,19 @@
 // some sums 
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main()
-// {
-//     int n, a, b, sum = 0, tsum = 0;
-//     cin >> n >> a >> b;
+int main()
+{
+    int n, a, b, sum = 0, tsum = 0;
+    cin >> n >> a >> b;
 
-//     int num = 0;
-//     while(num > 0) {
-//         sum += num % 10;
-//         num /= 10;
-//     }
-    
-//     for (int i = 1; i <= n; i++) {
-//         if (num >= a && num <= b) {
-//             tsum += i;
-//         }
-//     }
-//     cout << tsum << '\n';
-// }
-
-
-
-#include <iostream>
-
-bool isInRange(int num, int a, int b) {
-  int sumOfDigits = 0;
-  while (num > 0) {
-    sumOfDigits += num % 10;
-    num /= 10;
-  }
-  return a <= sumOfDigits && sumOfDigits <= b;
+    for (int i = 1;)
 }
 
-int main() {
-  int n, a, b;
-  std::cin >> n >> a >> b;
 
-  int sum = 0;
-  for (int i = 1; i <= n; ++i) {
-    if (isInRange(i, a, b)) {
-      sum += i;
-    }
-  }
 
-  std::cout << sum << std::endl;
 
-  return 0;
-}
 
 // #include <iostream>
 
