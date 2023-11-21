20b776a1e488f59412377877adf21e7476a9f396
0xsadik <torikussadik404@gmail.com>
2023-11-21 16:59:09 +0600

    <.>

--- codeforces/Loops/Y.cpp
+++ codeforces/Loops/Y.cpp
@@ -4,26 +4,11 @@
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
+    int n; cin >> n;
 
-    int fib[N];
-
-    // First two numbers in the Fibonacci sequence
-    fib[0] = 0;
-    fib[1] = 1;
-
-    // Generate the Fibonacci sequence
-    for (int i = 2; i < N; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-
-    // Print the first N numbers of the Fibonacci sequence
-    for (int i = 0; i < N; i++) {
-        cout << fib[i] << " ";
-    }
-
-    cout << endl;
+    int farr[n];
+    farr[0] = 0;
+    farr[1] = 1;
 
     return 0;
 }
