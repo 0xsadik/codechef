fb1a5439e18ef414b883cee052e28f67f7258f04
0xsadik <torikussadik404@gmail.com>
2023-11-21 14:32:33 +0600

    <.>

--- codeforces/Loops/R.cpp
+++ codeforces/Loops/R.cpp
@@ -5,14 +5,5 @@ using namespace std;
 
 int main()
 {
-    int n, m, sum = 0;
-    cin >> n >> m;
-
-    if (n > m) {
-        for (int i = m; i <= n; i++) {
-            cout << i << ' ';
-            sum += i;
-        }
-    }
-    cout << "sum =" << sum << '\n';
+    if ()
 }
