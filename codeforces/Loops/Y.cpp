9608da12a0e44736f670f326ecd7953ab4c70847
0xsadik <torikussadik404@gmail.com>
2023-11-21 17:01:45 +0600

    <.>

--- codeforces/Loops/Y.cpp
+++ codeforces/Loops/Y.cpp
@@ -1,22 +1,33 @@
 // Easy Fibonacci
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n; cin >> n;
+
+//     int farr[n];
+//     farr[0] = 0;
+//     farr[1] = 1;
+
+//     for (int i = 2; i < n; i++) {
+//         farr[i] = farr[i - 1] + farr[i - 2];
+//     }
+
+//     for (int i = 0; i < n; i++) {
+//         cout << farr[i] << ' ';
+//     }
+//     cout << '\n';
+
+//     return 0;
+// }
+
+
+
+#include<iostream>
 using namespace std;
 
-int main() {
-    int n; cin >> n;
-
-    int farr[n];
-    farr[0] = 0;
-    farr[1] = 1;
-
-    for (int i = 2; i < n; i++) {
-        farr[i] = farr[i - 1] + farr[i - 2];
-    }
-
-    for (int i = 0; i < n; i++) {
-        cout << farr[i] << ' ';
-    }
-    cout << '\n'
-    return 0;
+int main()
+{
+    
 }
