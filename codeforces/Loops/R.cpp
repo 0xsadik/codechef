7eb00392b55d82a7ac0791abb79ce49cd6966af5
0xsadik <torikussadik404@gmail.com>
2023-11-21 14:29:05 +0600

    <.>

---
+++ codeforces/Loops/R.cpp
@@ -0,0 +1,16 @@
+// sequence of numbers and sum 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, m, sum = 0;
+    cin >> n >> m;
+
+    if (n > m) {
+        for (int i = m; i <= n; i++) {
+            cout << i << '\n';
+        }
+    }
+}
