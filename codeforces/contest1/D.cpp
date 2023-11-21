acea8a6dfd12b6c2c35eb8545da545e4d18d5352
0xsadik <torikussadik404@gmail.com>
2023-11-21 18:25:05 +0600

    <.>

--- codeforces/contest1/D.cpp
+++ codeforces/contest1/D.cpp
@@ -114,7 +114,7 @@ using namespace std;
 int main() {
     long long a, b, c, d;
     cin >> a >> b >> c >> d;
-    int s1, s2, s3, s4, s5, s6, s7, s8;
+    int s1, s2, s3, s4, s5, s6, s7, s8, s9;
     s1 = a + b + c;
     s2 = a + b - c;
     s3 = a - b + c;
@@ -124,7 +124,7 @@ int main() {
     s7 = a / b * c;
     s8 = a / b / c;
     s9 = a + b * c;
-    if (s1 == d || s2 == d || s3 == d || s4 == d || s5 == d || s6 == d || s7 == d || s8 == d) {
+    if (s1 == d || s2 == d || s3 == d || s4 == d || s5 == d || s6 == d || s7 == d || s8 == d || s9 == d) {
         cout << "YES" << endl;
     } else {
         cout << "NO" << endl;
