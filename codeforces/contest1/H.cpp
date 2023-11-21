2ccd7899d06d58189caa7492cf76df1022f1fb0c
0xsadik <torikussadik404@gmail.com>
2023-11-21 18:50:06 +0600

    <.>

--- codeforces/contest1/H.cpp
+++ codeforces/contest1/H.cpp
@@ -1,5 +1,31 @@
 // Data type guessing 
 
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     double n, k, a;
+//     cin >> n >> k >> a;
+
+//     long long f = (k * a) / a;
+//     double q = (k * n) / a, t = q - f;
+//     if (t > 0) {
+//         cout << "double\n";
+//     }
+//     else if (q > 2147483647) {
+//         cout << "long long\n";
+//     }
+//     else {
+//         cout << "int\n";
+//     }
+
+//     return 0;
+// }
+
+
+
+
 #include<iostream>
 using namespace std;
 
@@ -8,17 +34,7 @@ int main()
     double n, k, a;
     cin >> n >> k >> a;
 
-    long long f = (k * a) / a;
-    double q = (k * n) / a, t = q - f;
-    if (t > 0) {
-        cout << "double\n";
-    }
-    else if (q > 2147483647) {
-        cout << "long long\n";
-    }
-    else {
-        cout << "int\n";
-    }
+    
 
     return 0;
 }
