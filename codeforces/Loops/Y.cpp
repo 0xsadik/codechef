8c1392c0af42accfad51fc6e02d6aca518b1514d
0xsadik <torikussadik404@gmail.com>
2023-11-21 16:58:17 +0600

    <.>

--- codeforces/Loops/Y.cpp
+++ codeforces/Loops/Y.cpp
@@ -1,17 +1,29 @@
 // Easy Fibonacci
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    int a = 0, b = 1;
-    for (int i = 0; i < n; i++) {
-        int tmp = a;
-        a = b;
-        b = tmp + b;
+int main() {
+    int N;
+    cin >> N;
+
+    int fib[N];
+
+    // First two numbers in the Fibonacci sequence
+    fib[0] = 0;
+    fib[1] = 1;
+
+    // Generate the Fibonacci sequence
+    for (int i = 2; i < N; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
     }
-    cout << ' ';
+
+    // Print the first N numbers of the Fibonacci sequence
+    for (int i = 0; i < N; i++) {
+        cout << fib[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
