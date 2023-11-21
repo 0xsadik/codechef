9f9f6e17a7d96063f70cde8523d88b270586023b
0xsadik <torikussadik404@gmail.com>
2023-11-21 17:03:29 +0600

    <.>

--- codeforces/Loops/Y.cpp
+++ codeforces/Loops/Y.cpp
@@ -29,6 +29,13 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+
     int v1 = 0, v2 = 1;
-    
+    for (int i = 0; i < n; i++) {
+        int tmp = v1;
+        v1 = v2;
+        v2 = tmp + v2;
+    }
 }
