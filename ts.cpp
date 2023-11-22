0dd0bab55acc7d20380af2274053235a95a9f5d6
0xsadik <torikussadik404@gmail.com>
2023-11-23 00:45:25 +0600

    <.>

---
+++ ts.cpp
@@ -0,0 +1,43 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int x;
+//     cin >> x;
+//     (x >= 12) ? cout << "YES\n" : cout << "NO\n";
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int t,s, twords;
+//     cin >> t;
+//     while(t--) {
+//         cin >> s;
+//         twords = 24 * 1000 * s;
+//         cout << twords << '\n';
+//     }
+//     return 0;
+// }
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, x, y, n;
+    cin >> t;
+    while(t--) {
+        cin >> x >> y;
+
+        n = x + y;
+        (y <= (n / 2)) ? cout << n << '\n' : cout << n + 2 * (y - (n / 2)) - 1 << '\n';
+    }
+}
