8797f7ea8b1ebe8773a10e3ecc800ab15331a310
0xsadik <torikussadik404@gmail.com>
2023-11-23 14:26:22 +0600

    <.>

--- codeforces/contest2/D.cpp
+++ codeforces/contest2/D.cpp
@@ -5,10 +5,15 @@ using namespace std;
 
 int main()
 {
-    int t;
+    int t, sum = 0;
     cin >> t;
 
     while(t--) {
-        
+        int l, r;
+        cin >> l >> r;
+
+        for (int i = l; i <= r; i++) {
+            
+        }
     }
 }
