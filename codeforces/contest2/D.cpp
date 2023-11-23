356bcd684d81495615f2f0314d29be0e15ec845f
0xsadik <torikussadik404@gmail.com>
2023-11-23 14:37:34 +0600

    <.>

--- codeforces/contest2/D.cpp
+++ codeforces/contest2/D.cpp
@@ -31,10 +31,13 @@ int main()
     int t;
     cin >> t;
     while(t--) {
-        long long l, r, m, mi;
+        long long l, r, m, mi, ans1, ans2;
         cin >> l >> r;
 
         m = max(l, r);
         mi = min(l, r);
+        mi--;
+        ans1 = mi * (mi + 1) / 2;
+        ans2 = m * (m + 1) / 2;
     }
 }
