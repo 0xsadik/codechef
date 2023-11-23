4a5fc9e555633a91732f8d49b2a6f6ec0ba40fbe
0xsadik <torikussadik404@gmail.com>
2023-11-23 14:44:31 +0600

    <.>

--- codeforces/contest2/F.cpp
+++ codeforces/contest2/F.cpp
@@ -1,5 +1,32 @@
 // Break Number 
 
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n; 
+//     cin >> n;
+
+//     int mf = 0;
+//     for (int i = 0; i < n; i++) {
+//         int x;
+//         cin >> x;
+
+//         int f = 0;
+//         while(x % 2 == 0) {
+//             x /= 2;
+//             f++;
+//         }
+//         mf = max(mf, f);
+//     }
+//     cout << mf << '\n';
+
+//     return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
 
@@ -9,7 +36,7 @@ int main()
     cin >> n;
 
     int mf = 0;
-    for (int i = 0; i < n; i++) {
+    while(n--) {
         int x;
         cin >> x;
 
