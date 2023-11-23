0c4cc40fc4a6d31e66a709877d9259e2ef2e4987
0xsadik <torikussadik404@gmail.com>
2023-11-23 14:41:55 +0600

    <.>

--- codeforces/contest2/F.cpp
+++ codeforces/contest2/F.cpp
@@ -5,5 +5,14 @@ using namespace std;
 
 int main()
 {
-    int 
+    int n; 
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        int f = 0;
+        
+    }
 }
