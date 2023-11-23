33f375d3b21ba7f05694c12b5cc21341915f8e18
0xsadik <torikussadik404@gmail.com>
2023-11-23 14:35:50 +0600

    <.>

--- codeforces/contest2/D.cpp
+++ codeforces/contest2/D.cpp
@@ -1,5 +1,28 @@
 // Range sum 
 
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int t;
+//     cin >> t;
+
+//     while(t--) {
+//         int l, r;
+//         cin >> l >> r;
+
+//         int sum = 0;
+//         for (int i = l; i <= r; i++) {
+//             sum += i;
+//         }
+//         cout << sum << '\n';
+//     }
+//     return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
 
@@ -7,16 +30,10 @@ int main()
 {
     int t;
     cin >> t;
-
     while(t--) {
         int l, r;
         cin >> l >> r;
 
-        int sum = 0;
-        for (int i = l; i <= r; i++) {
-            sum += i;
-        }
-        cout << sum << '\n';
+        
     }
-    return 0;
 }
