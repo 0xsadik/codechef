ed78228e648b5000b539171a018497d4097a0c76
0xsadik <torikussadik404@gmail.com>
2023-11-23 14:19:29 +0600

    <.>

--- codeforces/contest2/C.cpp
+++ codeforces/contest2/C.cpp
@@ -24,7 +24,7 @@ int main()
             }
         }
         count++;
-        if (count == 3 || i == ) {
+        if (count == k || i == n) {
             cout << min << ' ';
             count = 0;
             flag = 1;
