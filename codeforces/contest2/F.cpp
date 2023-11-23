b76f2928b4a4e596d55ecc3ebe7d7e77f5fe7fe9
0xsadik <torikussadik404@gmail.com>
2023-11-23 14:42:47 +0600

    <.>

--- codeforces/contest2/F.cpp
+++ codeforces/contest2/F.cpp
@@ -8,11 +8,16 @@ int main()
     int n; 
     cin >> n;
 
+    int mf = 0;
     for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
 
         int f = 0;
-        
+        while(x % 2 == 0) {
+            x /= 2;
+            f++;
+        }
+        mf = mx(max)
     }
 }
