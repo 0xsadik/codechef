8dd22d57e4069e660b4ee181d923a3afd51d8b7d
0xsadik <torikussadik404@gmail.com>
2023-11-25 20:18:51 +0600

    <.>

--- codeforces/Array/B.cpp
+++ codeforces/Array/B.cpp
@@ -7,7 +7,12 @@ using namespace std;
 int main()
 {
     int n;
-    int arr[]
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    long long x; cin >> x;
+    
 
     return 0;
 }
