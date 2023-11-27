9e1fe5e7d0f494ff1a97df2710634948ef372e33
0xsadik <torikussadik404@gmail.com>
2023-11-27 07:35:24 +0600

    <.>

--- codeforces/Array/B.cpp
+++ codeforces/Array/B.cpp
@@ -1,27 +1,10 @@
 
 // searching 
 
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    long long n;
-    long long arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    long long x; cin >> x;
-    bool flag = false;
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == x) {
-            cout << i << '\n';
-            flag = true;
-            break;
-        }
-    }
-    if (flag == false) {
-        cout << -1 << '\n';
-    }
-    return 0;
+    
 }
