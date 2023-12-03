e5a991de9e8c0c97184a0d58045f5c4e2d916c33
0xsadik <torikussadik404@gmail.com>
2023-12-03 21:40:40 +0600

    <.>

--- codeforces/Array/E.cpp
+++ codeforces/Array/E.cpp
@@ -1,46 +1,23 @@
-// lowest number 
-
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// int main()
-// {
-//     int n;
-//     cin >> n;
-//     int arr[n];
-//     int m = INT_MAX;
-//     for(int i = 1; i <= n; i++) {
-//         cin >> arr[i];
-//         m = min (m, arr[i]);
-//         if (arr[i] == m) {
-//             cout << arr[i] << ' ' << i;
-//             break;
-//         }
-//     }
-//     return 0;
-// }
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int n;
+    int n, mn, lpos;
     cin >> n;
-    int A[n];
-    int mipos = A[0];
-    int mindx = 0;
+    int arr[n];
     for (int i = 0; i < n; i++) {
-        cin >> A[i];
-
-        if (A[i] < mipos) {
-            mipos = A[i];
-            mindx = i;
+        cin >> arr[i];
+    }
+    mn = arr[0];
+    lpos = 1;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < mn) {
+            mn = arr[i];
+            lpos = i + 1;
         }
     }
-    cout << mipos << ' ' << mindx + 1 << '\n';
+    cout << mn << ' ' << lpos << '\n';
 
     return 0;
-
 }
---
+++ codeforces/EDUCFround159/A.binary_imbalance.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--) 
+    {
+        int n;
+        string s;
+
+        cin >> n;
+        cin >> s;
+
+        int zero = 0;
+        int one = 1;
+
+        for (char ch : s) {
+            (ch == '0') ? zero++ : one++;
+        }
+
+        if (zero > one) {
+            cout << "YES\n";
+        }
+        else {
+            bool isPossible = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (s[i] != s[i + 1]) {
+                    isPossible = true;
+                    break;
+                }
+            }
+            (isPossible) ? cout << "YES\n" : cout << "NO\n";
+        }
+    }
+    return 0;
+}
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     string s;
+
+//     cin >> n;
+//     cin >> s;
+
+//     int zero = 0;
+
+// }
+
+
+// https://youtu.be/sNrLlmOIn-c?t=683
